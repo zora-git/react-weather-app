92f701b022b60c73c5b7ecf5bd2b4f9e525eee69
zora-git <154859133+zora-git@users.noreply.github.com>
2024-02-10 21:41:29 +0900

    html structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./App.css";
+import Weather from "./Weather.js";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
+        <Weather />
         <footer>
           This project was coded by{" "}
           <a
---
+++ src/Weather.js
@@ -0,0 +1,53 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="weather-app">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city"
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Search" />
+          </div>
+        </div>
+      </form>
+      <h1>Seoul</h1>
+      <h3>Wednesday 7:00</h3>
+
+      <div className="row">
+        <div className="col-6">
+          <div className="row">
+            <div className="col-4">
+              <img
+                src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+                alt="Sunny"
+                className="main-icon"
+              />
+            </div>
+            <div className="col-4 main-temp">54</div>
+
+            <div className="col-4">
+              <div className="row celcius">°C</div>
+              <div className="row fahrenheit">°F</div>
+            </div>
+          </div>
+        </div>
+        <div class="col-6">
+          <h3>Mostly Cloudy</h3>
+          <ul>
+            <li>Precipitation:15%</li>
+            <li>Humidity: 72%</li>
+            <li>Wind: 13km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
