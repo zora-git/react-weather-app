afc9f23901c756b6f89eb059284d18567578c51f
zora-git <154859133+zora-git@users.noreply.github.com>
2024-02-10 19:55:14 +0900

    created footer

--- src/App.js
+++ src/App.js
@@ -2,7 +2,29 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://hjp-portfolio.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          HJ Park
+        </a>{" "}
+        and is {""}
+        <a
+          href="https://github.com/zora-git/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
