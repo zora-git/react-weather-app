5104feb3ea4e244b4df8ca7209d5bb5ac9d146e4
zora-git <154859133+zora-git@users.noreply.github.com>
2024-02-10 20:25:21 +0900

    Added boostrap

--- src/App.js
+++ src/App.js
@@ -4,25 +4,27 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://hjp-portfolio.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          HJ Park
-        </a>{" "}
-        and is {""}
-        <a
-          href="https://github.com/zora-git/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://hjp-portfolio.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            HJ Park
+          </a>{" "}
+          and is {""}
+          <a
+            href="https://github.com/zora-git/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
