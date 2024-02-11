93d2ebdf803f491996a177c1de0b305844e6081c
zora-git <154859133+zora-git@users.noreply.github.com>
2024-02-11 12:28:05 +0900

    updated responsive app

--- src/App.js
+++ src/App.js
@@ -5,27 +5,25 @@ import Weather from "./Weather.js";
 function App() {
   return (
     <div className="App">
-      <div className="container">
-        <Weather />
-        <footer>
-          This project was coded by{" "}
-          <a
-            href="https://hjp-portfolio.netlify.app/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            HJ Park
-          </a>{" "}
-          and is {""}
-          <a
-            href="https://github.com/zora-git/react-weather-app"
-            target="_blank"
-            rel="noreferrer"
-          >
-            open-sourced
-          </a>
-        </footer>
-      </div>
+      <Weather />
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://hjp-portfolio.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          HJ Park
+        </a>{" "}
+        and is {""}
+        <a
+          href="https://github.com/zora-git/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced
+        </a>
+      </footer>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Weather.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Weather() {
   return (
@@ -13,18 +14,25 @@ export default function Weather() {
               className="form-control"
             />
           </div>
-          <div className="col-3">
-            <input type="submit" value="Search" className="search" />
+          <div className="col-3 ">
+            <input
+              type="submit"
+              value="Search"
+              className="d-none d-md-block search"
+            />
           </div>
+        </div>
+        <div>
+          <FontAwesomeIcon icon="fa-solid fa-magnifying-glass" />
         </div>
       </form>
       <h1>Seoul</h1>
       <h3>Wednesday 7:00</h3>
 
       <div className="row details">
-        <div className="col-md-6 main">
+        <div className="col-md-6 main-column">
           <div className="row main-section">
-            <div className="col-md-6">
+            <div className="col-md-6 icon-column">
               <img
                 src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
                 alt="Sunny"
@@ -32,20 +40,20 @@ export default function Weather() {
               />
             </div>
 
-            <div className="col-6 temperature">
+            <div className="col-6 temp-column">
               <div class="row ">
-                <div className="col-10 main-temp">54</div>
-                <div className="col-2">
-                  <div class="row">C</div>
-                  <div class="row">F</div>
+                <div className="col-9 main-temp">54</div>
+                <div className="col-3 unit">
+                  <div class="row celcius">°C</div>
+                  <div class="row fahrenheit">°F</div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <div class="col-md-6">
+        <div className="col-md-6 description-column">
           <h4>Mostly Cloudy</h4>
-          <ul>
+          <ul className="description-list">
             <li>Precipitation:15%</li>
             <li>Humidity: 72%</li>
             <li>Wind: 13km/h</li>
